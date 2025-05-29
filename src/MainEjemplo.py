828a40e69ea226ffcee52aa5a16a8788a455e7e3
Hecta18 <62799494+Hecta18@users.noreply.github.com>
2025-05-28 19:32:22 -0600

    implementacion perfil en BD y recomendacion

--- src/MainEjemplo.py
+++ src/MainEjemplo.py
@@ -11,35 +11,43 @@ URI = "neo4j+s://33610e87.databases.neo4j.io" #aura
 USER = "neo4j"
 PASSWORD = os.getenv("PASSWORD")
 # metodo con archivo .env
-#os.environ.get("PASSWORD")
-# seguridad de contraseña con variable de entorno
-
 
 # Datos usuario
 nombre_usuario = 'Pedro'
-perfil_usuario = 'Influencer'
 comida_preferida = 'Hamburguesas'
 restaurante_preferido = 'Burger King'
-
 
 # Datos restaurante
 nombre_restaurante = 'Burger King'
 tipo_restaurante = 'Hamburguesas'
 calificacion_restaurante = 4.5
 
+# Datos perfil
+nombre_perfil = 'Influencer'
+calificacion_recomendacion = 4.0
+
 # queries
 create_usuario = """
-CREATE (u:Usuario {nombre: '""" + nombre_usuario + """', perfil: '""" + perfil_usuario + """'})
+CREATE (u:Usuario {nombre: '""" + nombre_usuario + """', comida: '""" + comida_preferida + """', restaurante: '""" + restaurante_preferido + """'})
 RETURN u
 """
 create_restaurant = """
 CREATE (r:Restaurante {nombre: '""" + nombre_restaurante + """', tipo: '""" + tipo_restaurante + """', calificacion: """ + str(calificacion_restaurante) + """})
 RETURN r
 """
-create_relation = """
+create_perfil = """
+CREATE (p:Perfil {nombre: '""" + nombre_perfil + """', calificacion: """ + str(calificacion_recomendacion) + """})
+RETURN p
+"""
+create_relation_order = """
 MATCH (r:Restaurante {nombre: '""" + nombre_restaurante + """'}), (u:Usuario {nombre: '""" + nombre_usuario + """'})
 CREATE (u)-[:PEDIR]->(r)
 RETURN u, r
+"""
+create_relation_be = """
+MATCH (p:Perfil {nombre: '""" + nombre_perfil + """'}), (u:Usuario {nombre: '""" + nombre_usuario + """'})
+CREATE (u)-[:ES]->(p)
+RETURN u, p
 """
 explicit_feedback_based = """
 MATCH (u:Usuario {nombre: '""" + nombre_usuario + """'})-[:PEDIR]->(r:Restaurante)<-[:PEDIR]-(otro:Usuario)-[:PEDIR]->(sugerido:Restaurante)
@@ -55,9 +63,9 @@ RETURN r.nombre
 LIMIT 5
 """
 profile_based = """
-MATCH (u:Usuario {nombre: '""" + nombre_usuario + """'})-[:ES]->(:Perfil {tipo: '""" + perfil_usuario + """'})
+MATCH (u:Usuario {nombre: '""" + nombre_usuario + """'})-[:ES]->(p:Perfil)
 MATCH (r:Restaurante)
-WHERE r.nuevo = true AND r.calificacion >= 4
+WHERE r.calificacion >= p.calificacion
 RETURN r.nombre
 LIMIT 5
 """
@@ -69,7 +77,9 @@ conn = Neo4jConnection(URI, USER, PASSWORD)
 try:
     print(queryWithoutResults(conn, create_usuario))
     print(queryWithoutResults(conn, create_restaurant))
-    print(queryWithoutResults(conn, create_relation))
+    print(queryWithoutResults(conn, create_perfil))
+    print(queryWithoutResults(conn, create_relation_order))
+    print(queryWithoutResults(conn, create_relation_be))
     # print(queryWithResults(conn, explicit_feedback_based))
     # print(queryWithResults(conn, content_based))
     # print(queryWithResults(conn, profile_based))
