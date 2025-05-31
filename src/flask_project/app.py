1543e4164ff71ca10520b9428a6c123aa9ca10e7
Hecta18 <62799494+Hecta18@users.noreply.github.com>
2025-05-31 17:22:22 -0600

    modularizacion

--- src/flask_project/app.py
+++ src/flask_project/app.py
@@ -1,92 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from lista_restaurantes import todos_restaurantes
 
 app = Flask(__name__)
 
 app.secret_key = "clave_secreta"
 # Configuración de la clave secreta para la sesión
-todos_restaurantes = [
-    {
-        "nombre": "Burger King",
-        "imagen": "https://www.pngall.com/wp-content/uploads/13/Burger-King-Logo-PNG-Clipart.png",
-        "tipo": "Hamburguesas",
-        "platos": [
-            {"nombre": "Whopper", "descripcion": "Hamburguesa con carne a la parrilla", "precio": 35},
-            {"nombre": "Papas grandes", "descripcion": "Papas fritas crujientes", "precio": 18}
-        ]
-    },
-    {
-        "nombre": "Pizza Hut",
-        "imagen": "https://1000marcas.net/wp-content/uploads/2020/01/Pizza-Hut-Logo-1999.jpg",
-        "tipo": "Pizza",
-        "platos": [
-            {"nombre": "Pizza Suprema", "descripcion": "Pizza con todo", "precio": 50},
-            {"nombre": "Pan de ajo", "descripcion": "Pan con mantequilla y ajo", "precio": 15}
-        ]
-    },
-    {
-        "nombre": "Sushi Itto",
-        "imagen": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEX4IpoSFlx0lmkGB4w5W1IY3XKC81wPW9Mw&s",
-        "tipo": "Sushi",
-        "platos": [
-            {"nombre": "Sushi variado", "descripcion": "Selección de sushi fresco", "precio": 60},
-            {"nombre": "Rollos de atún", "descripcion": "Rollos de atún con aguacate", "precio": 45}
-        ]
-    },
-    {
-        "nombre": "Domino's",
-        "imagen": "https://prestonchamber.org/wp-content/uploads/2024/06/dominos.png",
-        "tipo": "Pizza",
-        "platos": [
-            {"nombre": "Margarita", "descripcion": "Pizza clásica con tomate y queso", "precio": 40},
-            {"nombre": "Pepperoni", "descripcion": "Pizza con rodajas de pepperoni", "precio": 45}
-        ]
-    },
-    {
-        "nombre": "Subway",
-        "imagen": "https://m.media-amazon.com/images/G/01/AdProductsWebsite/images/CaseStudies/Subway_-_Thumbnail.jpg",
-        "tipo": "Sandwich",
-        "platos": [
-            {"nombre": "Sub de pollo", "descripcion": "Sándwich de pollo a la parrilla", "precio": 30},
-            {"nombre": "Sub vegetariano", "descripcion": "Sándwich con vegetales frescos", "precio": 25}
-        ]
-    },
-    {
-        "nombre": "KFC",
-        "imagen": "https://1000marcas.net/wp-content/uploads/2020/01/KFC-logo.png",
-        "tipo": "Hamburguesas",
-        "platos": [
-            {"nombre": "Pollo frito", "descripcion": "Pollo frito crujiente", "precio": 50},
-            {"nombre": "Tiras de pollo", "descripcion": "Tiras de pollo empanizadas", "precio": 35}
-        ]
-    },
-    {
-        "nombre": "Taco Bell",
-        "imagen": "https://cdn.worldvectorlogo.com/logos/taco-bell-7.svg",
-        "tipo": "Hamburguesas",
-        "platos": [
-            {"nombre": "Tacos al pastor", "descripcion": "Tacos con carne al pastor", "precio": 20},
-            {"nombre": "Burrito grande", "descripcion": "Burrito relleno de frijoles y carne", "precio": 30}
-        ]
-    },
-    {
-        "nombre": "Wendy's",
-        "imagen": "https://cdn.worldvectorlogo.com/logos/wendys-1.svg",
-        "tipo": "Hamburguesas",
-        "platos": [
-            {"nombre": 'Hamburguesa clásica', "descripcion": "Hamburguesa con lechuga y tomate", "precio": 28},
-            {"nombre": "Papas fritas", "descripcion": "Papas fritas crujientes", "precio": 15}
-        ]
-    },
-    {
-        "nombre": "McDonald's",
-        "imagen": "https://1000marcas.net/wp-content/uploads/2019/11/McDonalds-logo.png",
-        "tipo": "Hamburguesas",
-        "platos": [
-            {"nombre": "Big Mac", "descripcion": "Hamburguesa con dos carnes y salsa especial", "precio": 40},
-            {"nombre": "McFlurry", "descripcion": "Postre helado con trozos de chocolate", "precio": 20}
-        ]
-    },
-]
 
 @app.route("/")
 def index():
